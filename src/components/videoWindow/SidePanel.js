ea713f053ee4ee5494766557251982ec7104d167
liorbazak1710 <liorbazak99@gmail.com>
2024-08-24 12:14:08 +0300

    moved style to css and changed some style of the side panel

--- src/components/videoWindow/SidePanel.js
+++ src/components/videoWindow/SidePanel.js
@@ -15,7 +15,7 @@ const DraggableItem = ({ videoUrl, name }) => {
 
   return (
     <div ref={drag} className={`draggable-item ${isDragging ? 'dragging' : ''}`}>
-      <video src={videoUrl} controls width="150" height="100" />
+      <video src={videoUrl} controls width="155" height="100" />
       <p>{name}</p>
     </div>
   );
@@ -31,28 +31,6 @@ const SidePanel = () => {
     setIsArrowToLeft(!isArrowToLeft);
   };
 
-  const arrowStyle = {
-    position: 'absolute',
-    right: isOpen ? 'calc(100% - 235px)' : '220px', // Move arrow to the right when side panel is open
-    top: '50%',
-    transform: 'translateY(-50%)',
-    cursor: 'pointer',
-    transition: 'left 0.3s', // Change 'right' to 'left' for transition
-    zIndex: 3,
-  };
-
-  const sidePanelStyle = {
-    width: '200px',
-    height: '100%',
-    backgroundColor: 'white',
-    position: 'fixed',
-    top: '0',
-    left: isOpen ? '0' : '-200px', // Change 'right' to 'left' and adjust positions accordingly
-    transition: 'left 0.1s', // Change 'right' to 'left' for transition
-    zIndex: 2,
-    overflowY: 'auto', // Add overflow-y property for vertical scrolling
-  };
-
   useEffect(() => {
     // Fetch the video data from the JSON server
     axios.get('http://localhost:3000/movements') // Adjust the URL to your JSON server
@@ -66,13 +44,13 @@ const SidePanel = () => {
 
   return (
     <div className="side-panel">
-      <div style={arrowStyle} onClick={handleToggleSlideWindow}>
+      <div className={`arrow-icon ${isOpen ? 'arrow-open' : 'arrow-closed'}`} onClick={handleToggleSlideWindow}>
         {isOpen ? <SlArrowLeft /> : <SlArrowRight />}
       </div>
-      <div style={sidePanelStyle} className="slide-window-content">
+      <div className={`side-panel-style ${isOpen ? 'side-panel-open' : 'side-panel-closed'}`}>
         {isOpen && (
           <div>
-            <h3 className="text-movements-library">Movements Library</h3>
+            <h3 className="text-movements-library">Movements<br />Library</h3>
             <div className="video-list">
               {videos.map((video) => (
                 <DraggableItem key={video.id} videoUrl={video.videoUrl} name={video.name} />
